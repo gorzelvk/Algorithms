66c57538ebcae100afcecd38d972caf11338893b
Kacper Gorzelak <kacpergorzelak@outlook.com>
2023-04-02 20:27:18 +0200

    Initial commit

--- QuickSort/main.cpp
+++ QuickSort/main.cpp
@@ -1,5 +1,4 @@
 //
-//  main.cpp
 //  QuickSort
 //
 //  Created by Kacper Gorzelak on 02/04/2023.
@@ -8,7 +7,5 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
