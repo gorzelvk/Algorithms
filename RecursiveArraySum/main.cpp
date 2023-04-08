f42aef8a3a889b40a22febe04416035963065250
Kacper Gorzelak <kacpergorzelak@outlook.com>
2023-04-08 13:59:39 +0200

    Initial Commit

---
+++ RecursiveArraySum/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RecursiveArraySum
+//
+//  Created by Kacper Gorzelak on 08/04/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
