9af548a6ce35b46667263e976cf0fcf36f07b076
Kacper Gorzelak <kacpergorzelak@outlook.com>
2023-04-08 14:45:13 +0200

    Binary search algorithm

--- BinarySearch/main.cpp
+++ BinarySearch/main.cpp
@@ -6,16 +6,16 @@
 
 #include <iostream>
 
-int binarySearch(int numbers[], int arraySize, int soughtValue){
+int binary_search(int numbers[], int array_size, int sought_value){
     int low_index = 0;
-    int high_index = arraySize - 1;
+    int high_index = array_size - 1;
     int mid_index = (high_index - low_index)/2;
     while(true){
         mid_index = (high_index + low_index)/2;
-        if(numbers[mid_index] < soughtValue){
+        if(numbers[mid_index] < sought_value){
             low_index = mid_index + 1;
         }
-        else if (numbers[mid_index] > soughtValue){
+        else if (numbers[mid_index] > sought_value){
             high_index = mid_index - 1;
         }
         else{
@@ -26,6 +26,6 @@ int binarySearch(int numbers[], int arraySize, int soughtValue){
 
 int main(){
     int numbers[10] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-    std::cout << binarySearch(numbers, 10, 70) << std::endl; // returns index of sought Value
+    std::cout << binary_search(numbers, 10, 70) << std::endl; // returns index of sought Value
     return 0;
 }
